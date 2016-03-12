426960226977d42af5466a83e69d5e4ab8b5bceb
Optimus Applications <shinteo5@gmail.com>
2016-03-12 17:25:20 +0800

    Added ability to spawn a weapon into the game. Requires them to all have prefab (game mesh) added to the game.

---
+++ ItemSystem/Assets/Scenes/Script/Demo1.cs
@@ -0,0 +1,48 @@
+﻿//connect to database
+//Spawn item from the database
+
+
+
+using UnityEngine;
+using System.Collections;
+using RPGSystem.ItemSystem;
+
+
+//Only allow one component
+[DisallowMultipleComponent]
+public class Demo1 : MonoBehaviour
+{
+	public ISWeaponDatabase weaponDatabase;
+
+
+
+	void OnGUI()
+	{
+		for (int cnt = 0; cnt < weaponDatabase.Count; cnt++)
+			if (GUILayout.Button("Spawn: " + weaponDatabase.Get(cnt).ItemName))
+		{
+			Spawn(cnt);
+		}
+	}
+
+
+
+	void Spawn(int index)
+	{
+		ISWeapon isw = weaponDatabase.Get(index);
+
+		GameObject weapon = Instantiate(isw.Prefab);
+		weapon.name = isw.ItemName;
+
+		Weapon myWeapon = weapon.AddComponent<Weapon>();
+
+		myWeapon.WeaponIcon 		= isw.ItemIcon;
+		myWeapon.Value 				= isw.ItemValue;
+		myWeapon.Weight 			= isw.ItemWeight;
+		myWeapon.QualityIcon 		= isw.ItemQuality.Icon;
+		myWeapon.Min_Damage 		= isw.MinDamage;
+		myWeapon.Durability 		= isw.Durability;
+		myWeapon.Max_Durabilty 		= isw.MaxDurability;
+		myWeapon.EquipmentSlot 		= isw.equipmentSlot;
+	}
+}
---
+++ ItemSystem/Assets/Scenes/Script/weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using RPGSystem.ItemSystem;
+
+[DisallowMultipleComponent]
+public class Weapon : MonoBehaviour 
+{
+	public Sprite 
+			WeaponIcon,
+			QualityIcon;
+
+	public int 
+			Value,
+			Weight,
+			Min_Damage,
+			Durability,
+			Max_Durabilty;
+
+	public EquipmentSlot EquipmentSlot;
+}
